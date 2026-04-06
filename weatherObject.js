a5ee15be34b3426f9b02501823f8fad381955002
Azure1709 <bui.azurethanh@gmail.com>
2026-04-06 22:54:00 +0700

    Ex3: Weather Object

---
+++ weatherObject.js
@@ -0,0 +1,8 @@
+const currentWeather = {
+  temperature: 72,
+  conditions: "partly cloudy"
+};
+
+console.log(currentWeather.temperature);
+
+console.log(`It's ${currentWeather.temperature} degrees F and ${currentWeather.conditions}.`);
